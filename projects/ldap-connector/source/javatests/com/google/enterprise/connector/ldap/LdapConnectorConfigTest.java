aa64111a9e4bd2b2745fde5832caef41f46f57df
Max Ziff <donald.ziff@gmail.com>
2010-08-10 22:54:03 +0000

    http://codereview.appspot.com/1871059

---
+++ projects/ldap-connector/source/javatests/com/google/enterprise/connector/ldap/LdapConnectorConfigTest.java
@@ -0,0 +1,77 @@
+// Copyright 2010 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.ldap;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+
+import junit.framework.TestCase;
+
+import java.util.Set;
+
+public class LdapConnectorConfigTest extends TestCase {
+
+  public void testSimple() {
+    ImmutableMap<String, String> configMap =
+        ImmutableMap.<String, String> builder().
+        put(LdapConnectorConfig.ConfigName.AUTHTYPE.toString(), "ANONYMOUS").
+        put(LdapConnectorConfig.ConfigName.HOSTNAME.toString(), LdapHandlerTest.getHostname()).
+        put(LdapConnectorConfig.ConfigName.METHOD.toString(), "STANDARD").
+        put(LdapConnectorConfig.ConfigName.BASEDN.toString(),
+        LdapHandlerTest.getTestResourceBundle().getString("basedn")).
+        put(LdapConnectorConfig.ConfigName.FILTER.toString(), LdapHandlerTest.getTestFilter()).
+        put(LdapConnectorConfig.ConfigName.SCHEMA.toString() + "_0", "foo").
+        put(LdapConnectorConfig.ConfigName.SCHEMA.toString() + "_1", "bar").
+        put(LdapConnectorConfig.ConfigName.SCHEMA.toString() + "_7", "baz").
+        build();
+    LdapConnectorConfig ldapConnectorConfig = new LdapConnectorConfig(configMap);
+    Set<String> schema = ldapConnectorConfig.getSchema();
+    assertTrue(schema.contains("foo"));
+    assertTrue(schema.contains("bar"));
+    assertTrue(schema.contains("baz"));
+    assertEquals("dn", ldapConnectorConfig.getSchemaKey());
+    assertTrue(schema.contains("dn"));
+  }
+
+  public void testBigSchema() {
+    // this test shows that a really big schema (bigger than MAX_SCHEMA_ELEMENTS) will
+    // be silently truncated, and that there's always room for the schema_key
+    // (even if it wasn't explicitly specified in the schema)
+    Builder<String, String> builder = ImmutableMap.<String, String> builder();
+    builder.
+        put("authtype", "ANONYMOUS").
+        put("hostname", LdapHandlerTest.getHostname()).
+        put("method", "STANDARD").
+        put("basedn", LdapHandlerTest.getTestResourceBundle().getString("basedn")).
+        put("filter", LdapHandlerTest.getTestFilter());
+    int extraElements = 10;
+    for (int i = 0; i < LdapConnectorConfig.MAX_SCHEMA_ELEMENTS + extraElements; i++) {
+      String key = LdapConnectorConfig.ConfigName.SCHEMA.toString() + "_" + i;
+      builder.put(key, key);
+    }
+    ImmutableMap<String, String> configMap = builder.build();
+    LdapConnectorConfig ldapConnectorConfig = new LdapConnectorConfig(configMap);
+    Set<String> schema = ldapConnectorConfig.getSchema();
+    assertEquals("dn", ldapConnectorConfig.getSchemaKey());
+    assertTrue(schema.contains("dn"));
+    String lastExpectedSchemaKey = LdapConnectorConfig.ConfigName.SCHEMA.toString() + "_" +
+        (LdapConnectorConfig.MAX_SCHEMA_ELEMENTS - 1);
+    String expectedDroppedSchemaKey = LdapConnectorConfig.ConfigName.SCHEMA.toString() + "_" +
+        (LdapConnectorConfig.MAX_SCHEMA_ELEMENTS + extraElements - 1);
+    assertTrue(schema.contains(lastExpectedSchemaKey));
+    assertFalse(schema.contains(expectedDroppedSchemaKey));
+    assertEquals(LdapConnectorConfig.MAX_SCHEMA_ELEMENTS + 1, schema.size());
+  }
+}
