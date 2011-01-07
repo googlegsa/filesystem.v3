1b5cf0e0879bcfbacb86562450e014b9d706c679
Vishvesh Mulay <vishveshmulay@gmail.com>
2011-01-07 19:27:12 +0000

    http://codereview.appspot.com/3848043/

--- projects/ldap-connector/source/javatests/com/google/enterprise/connector/ldap/ConnectorFieldsTest.java
+++ projects/ldap-connector/source/javatests/com/google/enterprise/connector/ldap/ConnectorFieldsTest.java
@@ -235,7 +235,7 @@ public class ConnectorFieldsTest extends TestCase {
   private static final String HTML_PREFIX =
       "<!DOCTYPE html PUBLIC "
       + "\"-//W3C//DTD XHTML 1.0 Strict//EN\" "
-      + "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">"
+      + "\"./third_party/test/dtd/xhtml1-strict.dtd\">"
       + "<html xmlns=\"http://www.w3.org/1999/xhtml\">"
       + "<head><title/></head><body><table>";
 
