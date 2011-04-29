1896705aaa2c2a5b54e5c6456420ae1f34dc1df2
Vishvesh Mulay <vishveshmulay@gmail.com>
2011-04-29 21:31:52 +0000

    temp commit. Actual CL URL will be added shortly

---
+++ projects/file-system-connector/source/java/com/google/enterprise/connector/filesystem/FileSystemConnectorErrorMessages.java
@@ -0,0 +1,21 @@
+// Copyright 2011 Google Inc. All Rights Reserved.
+
+package com.google.enterprise.connector.filesystem;
+
+
+/**Enum of error messages that are shown back to the user on admin console
+ *
+ */
+enum FileSystemConnectorErrorMessages {
+  CONNECTOR_INSTANTIATION_FAILED,
+  MISSING_FIELDS,
+  READ_START_PATH_FAILED,
+  PATTERNS_ELIMINATED_START_PATH,
+  ADD_ANOTHER_ROW_BUTTON,
+  CANNOT_ADD_ANOTHER_ROW,
+  ACCESS_DENIED,
+  WRONG_SMB_TYPE,
+  UNABLE_TO_ACCESS,
+  UNKNOWN_FILE_SYSTEM,
+  UNC_NEEDS_TRANSLATION;
+}
