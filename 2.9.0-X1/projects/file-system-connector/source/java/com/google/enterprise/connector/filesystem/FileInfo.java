3572a7780249fa805cda936527b5f0f0c6e19d13
Brett Michael Johnson <Brett.Michael.Johnson@gmail.com>
2012-02-06 20:21:13 +0000

    Tagging 2.9.0-X1 Build off of Lister/Retriever Dev branch

---
+++ 2.9.0-X1/projects/file-system-connector/source/java/com/google/enterprise/connector/filesystem/FileInfo.java
@@ -0,0 +1,66 @@
+// Copyright 2009 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.filesystem;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * A very minimal interface for information about a file.
+ *
+ */
+public interface FileInfo {
+  /**
+   * @return the kind of file system this file belongs to. E.g., SMB, JAVA, etc.
+   */
+  public String getFileSystemType();
+
+  /**
+   * <p>Lexicographic ordering of the paths within a directory tree must be
+   * consistent with in-order, depth-first traversal of that directory tree..
+   * This is tricky, because simple lexicographic ordering of paths doesn't
+   * quite work. To see why, suppose that a directory contains files named "abc"
+   * and "foo.bar" and a directory named "foo". Also suppose that "foo" contains
+   * a file named "x". If the file separator is "/", lexicographic ordering
+   * would be {abc, foo, foo.bar, foo/x}, but this is inconsistent with a
+   * depth-first scan. (For many file systems, one way to avoid this problem is
+   * to append the separator to directories before sorting.)
+   *
+   * @return file system path to this file.
+   */
+  public String getPath();
+
+  /**
+   * @return true if this is a directory.
+   */
+  public boolean isDirectory();
+
+  /**
+   * @return true if this is a regular file
+   */
+  public boolean isRegularFile();
+
+  /**
+   * @return the time this file was last modified
+   * @throws IOException if the modification time cannot be obtained
+   */
+  public long getLastModified() throws IOException;
+
+  /**
+   * Returns a {@link Acl} for this file or directory.
+   * @throws IOException
+   */
+  public Acl getAcl() throws IOException;
+}
---
+++ 2.9.0-X1/projects/file-system-connector/source/java/com/google/enterprise/connector/filesystem/PathParser.java
@@ -0,0 +1,70 @@
+// Copyright 2009 Google Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.filesystem;
+
+import com.google.enterprise.connector.spi.RepositoryDocumentException;
+
+/**
+ * This class provides a single method, {@link #getFile(String, Credentials)},
+ * which parses a path and returns a ReadonyFile. The returned file must
+ * actually exist and be readable. It is used to parse start paths.
+ *
+ */
+public class PathParser {
+  private final FileSystemTypeRegistry fileSystemTypeRegisty;
+
+  public PathParser(FileSystemTypeRegistry fileSystemTypeRegistry) {
+    this.fileSystemTypeRegisty = fileSystemTypeRegistry;
+  }
+
+  /**
+   * @param path A file-system dependent path.
+   * @param credentials for accessing the file. Ignored for native files.
+   * @return a readable file
+   * @throws RepositoryDocumentException
+   */
+  public ReadonlyFile<?> getFile(String path, Credentials credentials)
+      throws UnknownFileSystemException, RepositoryDocumentException {
+    for (FileSystemType fileSystemType : fileSystemTypeRegisty) {
+      if (fileSystemType.isPath(path)) {
+        ReadonlyFile<?> file = fileSystemType.getReadableFile(path, credentials);
+        if(!file.exists()) {
+          throw new NonExistentResourceException("Path does not exist: " + path);
+        } else if (!file.canRead()) {
+          throw new InsufficientAccessException("User doesn't have access to : " + path);
+        }
+        return file;
+      }
+    }
+    // Cannot find anything.
+    throw new UnknownFileSystemException("path does not match known file system: " + path);
+  }
+  
+  /**
+   * Returns whether the credentials are required to crawl the given start path.
+   * @param path path to crawl
+   * @return true / false depending on whether the credentials are required or not.
+   */
+  public boolean isUserNamePasswordNeeded(String path) {
+    for (FileSystemType fileSystemType : fileSystemTypeRegisty) {
+      if (fileSystemType.isPath(path)) {
+        if (fileSystemType.isUserPasswordRequired()) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+}
