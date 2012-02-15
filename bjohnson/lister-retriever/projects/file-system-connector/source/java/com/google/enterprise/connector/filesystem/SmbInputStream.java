b602fa0032205d74754e1be4121043936022f1d0
Brett Michael Johnson <Brett.Michael.Johnson@gmail.com>
2012-02-15 04:06:21 +0000

    Lister/Retriever Conversion - SmbInputStream derives from FilterInputStream
    Make SmbInputStream subclass FilterInputStream rather than BufferedInputStream.
    The backing SmbFileInputStream has a (configurable) 60KB input buffer.
    There is no need to wrap it in a BufferedInputStream with an 8KB buffer.
    
    Change Log:
    ----------
    M projects/file-system-connector/source/java/com/google/enterprise/connector/filesystem/SmbInputStream.java
    
    Code Review:
    -----------

--- bjohnson/lister-retriever/projects/file-system-connector/source/java/com/google/enterprise/connector/filesystem/SmbInputStream.java
+++ bjohnson/lister-retriever/projects/file-system-connector/source/java/com/google/enterprise/connector/filesystem/SmbInputStream.java
@@ -1,45 +1,53 @@
 // Copyright 2011 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package com.google.enterprise.connector.filesystem;
 
 import jcifs.smb.SmbException;
 import jcifs.smb.SmbFile;
 
-import java.io.BufferedInputStream;
+import java.io.FilterInputStream;
 import java.io.IOException;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * Wrapper Input stream for smbfile so that it can 
- * reset the last access time 
+ * Wrapper InputStream for SmbFile so that it can reset the last access time.
  */
-public class SmbInputStream extends BufferedInputStream {
+public class SmbInputStream extends FilterInputStream {
+  /** Standard logger. */
+  private static final Logger LOG =
+      Logger.getLogger(SmbInputStream.class.getName());
 
-  /**
-   *  whether or not to reset the last access time
-   */
+  /** Whether or not to reset the last access time. */
   private final boolean lastAccessTimeResetFlag;
-  /**
-   * last access time of the file represented in long
-   */
+
+  /** Last access time of the file represented in long. */
   private final long lastAccessTime;
-  /**
-   * SmbFile instance whose last access needs to be reset
-   */
+
+  /** SmbFile instance whose last access needs to be reset. */
   private final SmbFile delegate;
-  /**
-   * Standard logger
-   */
-  private static final Logger LOG = Logger.getLogger(SmbInputStream.class.getName());
+
   /**
    * @param delegate SmbFile instance whose last access needs to be reset
    * @param lastAccessTimeResetFlag whether or not to reset the last access time
    * @param lastAccessTime last access time of the file represented in long
    * @throws IOException in case of I/O failure
    */
-  public SmbInputStream(SmbFile delegate, boolean lastAccessTimeResetFlag, long lastAccessTime) throws IOException {
+  public SmbInputStream(SmbFile delegate, boolean lastAccessTimeResetFlag,
+                        long lastAccessTime) throws IOException {
     super(delegate.getInputStream());
     this.delegate = delegate;
     this.lastAccessTimeResetFlag = lastAccessTimeResetFlag;
@@ -48,7 +56,7 @@ public class SmbInputStream extends BufferedInputStream {
       SmbReadonlyFile.addToMap(delegate.getPath(), this);
     }
   }
-  
+
   /**
    * @return the lastAccessTime
    */
@@ -66,16 +74,17 @@ public class SmbInputStream extends BufferedInputStream {
   }
 
   /**
-   * This method sets the last access time back to the file
-   * Uses the API from the modified JCIFS jar file
+   * This method sets the last access time back to the file,
+   * using the API from the modified JCIFS jar file.
    */
   private void setLastAccessTime() {
-    LOG.finest("Setting last access time for : " + this.delegate.getPath() + " as : "
-            + new Date(this.lastAccessTime));
+    LOG.finest("Setting last access time for " + delegate.getPath()
+               + " as " + new Date(lastAccessTime));
     try {
-      delegate.setLastAccess(this.lastAccessTime);
+      delegate.setLastAccess(lastAccessTime);
     } catch (SmbException e) {
-      LOG.log(Level.WARNING,"Couldn't set the last access time for : " + delegate.getPath(), e);
+      LOG.log(Level.WARNING, "Could not set the last access time for "
+              + delegate.getPath(), e);
     }
   }
 }
