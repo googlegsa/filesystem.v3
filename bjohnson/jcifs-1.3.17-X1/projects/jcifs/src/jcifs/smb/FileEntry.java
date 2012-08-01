00872fc04ef4d32ce21fe099b9a767c9eaee3e7e
Brett Michael Johnson <Brett.Michael.Johnson@gmail.com>
2012-08-01 00:15:59 +0000

    Creating dev branch for jcifs 1.3.17 merge

---
+++ bjohnson/jcifs-1.3.17-X1/projects/jcifs/src/jcifs/smb/FileEntry.java
@@ -0,0 +1,12 @@
+package jcifs.smb;
+
+public interface FileEntry {
+
+    String getName();
+    int getType();
+    int getAttributes();
+    long createTime();
+    long lastModified();
+    long length();
+    long lastAccess();
+}
