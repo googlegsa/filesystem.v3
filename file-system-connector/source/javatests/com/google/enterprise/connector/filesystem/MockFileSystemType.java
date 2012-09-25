1f0fe4162139986145e8c73b6549ec6caac0070e
John Lacey <jlacey@google.com>
2012-09-25 00:02:55 +0000

    Tagged the 2.8.6 release from tags/builds/2.8.6-RC1.

---
+++ file-system-connector/source/javatests/com/google/enterprise/connector/filesystem/MockFileSystemType.java
@@ -0,0 +1,83 @@
+// Copyright 2009 Google Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.filesystem;
+
+import com.google.enterprise.connector.spi.RepositoryDocumentException;
+
+/**
+ */
+public class MockFileSystemType implements FileSystemType {
+  private final MockReadonlyFile root;
+  private final boolean isUserPasswordRequired;
+  
+  public MockFileSystemType(MockReadonlyFile root) {
+    this(root, false);
+  }
+
+  public MockFileSystemType(MockReadonlyFile root, boolean isUserPasswordRequired) {
+    this.root = root;
+    this.isUserPasswordRequired = isUserPasswordRequired;
+  }
+  
+  /* @Override */
+  public boolean isPath(String path) {
+    return path.startsWith(root.getPath());
+  }
+
+  /* @Override */
+  public MockReadonlyFile getFile(String path, Credentials credentials)
+      throws RepositoryDocumentException {
+    if (path.equals(root.getPath())) {
+      return root;
+    }
+    if (!path.startsWith(root.getPath())) {
+      throw new RepositoryDocumentException("no such file or directory: " + path);
+    }
+    String relativePath = path.substring(root.getPath().length());
+    String[] names = relativePath.split("/");
+
+    MockReadonlyFile result = root;
+    for (String name : names) {
+      if (name.length() != 0) {
+        if (result.get(name) == null) {
+          throw new RepositoryDocumentException("no such file or directory: " + result.getPath()
+              + "/" + name);
+        }
+        result = result.get(name);
+      }
+    }
+    return result;
+  }
+
+  /* @Override */
+  public String getName() {
+    return "mock";
+  }
+
+  /* @Override */
+  public MockReadonlyFile getReadableFile(String path, Credentials credentials)
+      throws RepositoryDocumentException {
+    MockReadonlyFile result = getFile(path, credentials);
+    if (!result.canRead()) {
+      throw new RepositoryDocumentException("failed to open file: " + path);
+    }
+    return result;
+  }
+
+  /* @Override */
+  public boolean isUserPasswordRequired() {
+    return isUserPasswordRequired;
+  }
+}
