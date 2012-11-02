d045c05566d43852f84d62c240dc2027e928aca9
Brett Michael Johnson <Brett.Michael.Johnson@gmail.com>
2012-11-02 05:12:47 +0000

    Tagging jcifs-1.3.17.4-X2 build

---
+++ 3.0.x/jcifs-1.3.17.4-X2/projects/file-system-connector/source/java/com/google/enterprise/connector/filesystem/FilesystemRepositoryDocumentException.java
@@ -0,0 +1,63 @@
+// Copyright 2010 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.filesystem;
+
+import com.google.enterprise.connector.spi.RepositoryDocumentException;
+
+/**
+ * Base exception for all scenarios where user will be shown some error message
+ * The main purpose of this exception is to control the behavior
+ * (getting the error message and passing it to UI) of all  
+ * exceptions of this type at common level.  
+ *
+ */
+public class FilesystemRepositoryDocumentException extends RepositoryDocumentException {
+  
+    /**
+     * Error message to show for this exception
+     */
+    private FileSystemConnectorErrorMessages errorMessage;
+    
+    /**
+     * Constructs a new DirectoryListingException with message and cause.
+     *
+     * @param message the message to be logged
+     * @param error Error message to show to the user
+     * @param cause root failure cause
+     */
+    public FilesystemRepositoryDocumentException(String message, FileSystemConnectorErrorMessages error, Throwable cause) {
+      super(message, cause);
+      this.errorMessage = error;
+    }
+
+    /**
+     * Constructs a new DirectoryListingException with message and cause.
+     *
+     * @param message the message to be logged
+     * @param error Error message to show to the user
+     */
+    public FilesystemRepositoryDocumentException(String message, FileSystemConnectorErrorMessages error) {
+      super(message);
+      this.errorMessage = error;
+    }
+
+    /**
+     * @return the errorMessage
+     */
+    public FileSystemConnectorErrorMessages getErrorMessage() {
+      return errorMessage;
+    }
+    
+}
---
+++ 3.0.x/jcifs-1.3.17.4-X2/projects/file-system-connector/source/javatests/com/google/enterprise/connector/filesystem/FilePatternMatcherTest.java
@@ -0,0 +1,58 @@
+// Copyright 2009 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.filesystem;
+
+import junit.framework.TestCase;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ */
+public class FilePatternMatcherTest extends TestCase {
+
+  public void testBasics() {
+    List<String> include = Arrays.asList("smb://foo.com/", "/foo/bar/");
+    List<String> exclude = Arrays.asList("smb://foo.com/secret/", "/foo/bar/hidden/");
+    FilePatternMatcher matcher = new FilePatternMatcher(include, exclude);
+
+    assertTrue(matcher.acceptName("smb://foo.com/baz.txt"));
+    assertTrue(matcher.acceptName("/foo/bar/baz.txt"));
+    assertFalse(matcher.acceptName("smb://notfoo/com/zippy"));
+    assertFalse(matcher.acceptName("smb://foo.com/secret/private_key"));
+    assertFalse(matcher.acceptName("/foo/bar/hidden/porn.png"));
+    assertFalse(matcher.acceptName("/bar/foo/public/knowledge"));
+  }
+
+  /* Limit this test to local file systems. Specifically, do not use
+   * SmbReadonlyFile for this, because it will try to verify the server.
+   * All the current ReadonlyFile implementations inherit the same
+   * acceptedBy() method from AbstractReadonlyFile anyway, so just one
+   * will do.
+   */
+  public void testReadonlyFileAcceptedBy() throws Exception {
+    List<String> include = Collections.singletonList("/foo/bar/");
+    List<String> exclude = Collections.singletonList("/foo/bar/hidden/");
+    FilePatternMatcher matcher = new FilePatternMatcher(include, exclude);
+    // We do not really need a FileSystemType for these, so null should be OK.
+    assertTrue(new JavaReadonlyFile(null, "/foo/bar/baz.txt")
+        .acceptedBy(matcher));
+    assertFalse(new JavaReadonlyFile(null, "/foo/bar/hidden/porn.png")
+        .acceptedBy(matcher));
+    assertFalse(new JavaReadonlyFile(null, "/bar/foo/public/knowledge")
+        .acceptedBy(matcher));
+  }
+}
