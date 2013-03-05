f858f318303572217aa58d93cd03967df4c40dcc
Brett Michael Johnson <Brett.Michael.Johnson@gmail.com>
2013-03-05 03:04:45 +0000

    Tagging 3.0.6-RC2 build.

---
+++ 3.0.x/3.0.6-RC2/projects/file-system-connector/source/java/com/google/enterprise/connector/filesystem/SmbAclBuilder.java
@@ -0,0 +1,183 @@
+// Copyright 2009 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.filesystem;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+
+import jcifs.smb.ACE;
+import jcifs.smb.SmbException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class SmbAclBuilder extends AbstractSmbAclBuilder {
+  private static final Logger LOGGER = Logger.getLogger(
+      SmbAclBuilder.class.getName());
+
+  /**
+   * The security ACEs for this file.
+   */
+  private final ACE[] securityAces;
+
+  /**
+   * True if this file has any inherited ACEs.
+   */
+  private final boolean hasInheritedAces;
+
+  /**
+   * Returns true if the associated {@link ACE} could be inherited by
+   * containers from which files will eventually inherit permissions.
+   * Note that we exclude NO_PROPAGATE ACEs, since those ACEs would never get
+   * propagated to a regular file.  We exclude CONTAINER_INHERIT ACEs, since
+   * they would never actually get inherited by a file.  We also exclude ACEs
+   * INHERITED from up the chain.
+   */
+  protected static Predicate<ACE> isContainerInheritAce = new Predicate<ACE>() {
+    public boolean apply(ACE ace) {
+      int mask = ACE.FLAGS_OBJECT_INHERIT | ACE.FLAGS_INHERITED
+                 | ACE.FLAGS_NO_PROPAGATE;
+      return (ace.getFlags() & mask) == ACE.FLAGS_OBJECT_INHERIT;
+    }
+  };
+
+  /**
+   * Returns true if the associated {@link ACE} could be inherited by
+   * regular files. Note that we exclude ACEs INHERITED from up the chain.
+   */
+  protected static Predicate<ACE> isObjectInheritAce = new Predicate<ACE>() {
+    public boolean apply(ACE ace) {
+      int mask = ACE.FLAGS_OBJECT_INHERIT | ACE.FLAGS_INHERITED;
+      return (ace.getFlags() & mask) == ACE.FLAGS_OBJECT_INHERIT;
+    }
+  };
+
+  /** Returns true if the associated {@link ACE} is INHERITED. */
+  protected static Predicate<ACE> isInheritedAce = new Predicate<ACE>() {
+    public boolean apply(ACE ace) {
+      return (ace.getFlags() & ACE.FLAGS_INHERITED) == ACE.FLAGS_INHERITED;
+    }
+  };
+
+  /**
+   * Returns true if the associated {@link ACE} is explicit for this
+   * node, not inherited from another node.
+   */
+  protected static Predicate<ACE> isDirectAce = new Predicate<ACE>() {
+    public boolean apply(ACE ace) {
+      return (ace.getFlags() & ACE.FLAGS_INHERITED) == 0;
+    }
+  };
+
+  /**
+   * Creates an {@link SmbAclBuilder}.
+   *
+   * @param file the {@link SmbFileDelegate} whose {@link Acl} we build.
+   * @param propertyFetcher Object containing the required properties.
+   */
+  SmbAclBuilder(SmbFileDelegate file, AclProperties propertyFetcher)
+      throws IOException {
+    super(file, propertyFetcher);
+
+    // Get the security ACEs on the file, if not already available.
+    // Also determines if any of those ACEs are inherited.
+    securityAces = file.getSecurity();
+    boolean hasInheritedAces = false;
+    if (securityAces != null) {
+      for (ACE ace : securityAces) {
+        if (isInheritedAce.apply(ace)) {
+          hasInheritedAces = true;
+          break;
+        }
+      }
+    }
+    this.hasInheritedAces = hasInheritedAces;
+  }
+
+  @Override
+  public Acl getAcl() throws IOException {
+    return getAcl(securityAces, isDirectAce, "");
+  }
+
+  @Override
+  public Acl getContainerInheritAcl() throws IOException {
+    return getAcl(securityAces, isContainerInheritAce, "Inheritable Directory");
+  }
+
+  @Override
+  public Acl getFileInheritAcl() throws IOException {
+    return getAcl(securityAces, isObjectInheritAce, "Inheritable File");
+  }
+
+  @Override
+  public Acl getInheritedAcl() throws IOException {
+    return (hasInheritedAces)
+        ? getAcl(securityAces, isInheritedAce, "Inherited") : null;
+  }
+
+  @Override
+  public Acl getShareAcl() throws IOException {
+    // SmbFile.getShareSecurity with true argument attempts to resolve
+    // the SIDs within each ACE form.
+    return getAcl(file.getShareSecurity(true), Predicates.<ACE>alwaysTrue(),
+                  "Share");
+  }
+
+  private Acl getAcl(ACE[] aces, Predicate<ACE> predicate, String type)
+      throws IOException {
+    if (LOGGER.isLoggable(Level.FINEST)) {
+      LOGGER.log(Level.FINEST, "{0} ACEs for {1}: {2}",
+          new Object[] { type, file, Arrays.toString(securityAces)});
+    }
+
+    List<ACE> fileAllowAces = new ArrayList<ACE>();
+    List<ACE> fileDenyAces = new ArrayList<ACE>();
+    checkAndAddAces(aces, fileAllowAces, fileDenyAces, predicate);
+
+    Acl acl = getAclFromAceList(fileAllowAces, fileDenyAces);
+    LOGGER.log(Level.FINEST, "{0} ACL for {1}: {2}",
+        new Object[] { type, file, acl });
+    return acl;
+  }
+
+  /**
+   * Gets the Allow and Deny ACEs from the {@code securityAces} and
+   * adds them to the repective list if they pass the {@code predicate}.
+   *
+   * @param securityAces
+   * @param aceList List where the allow ACEs are to be added.
+   * @param aceDenyList List where the deny ACEs are to be added.
+   * @param predicate decides whether to add the ACE or not.
+   * @throws IOException
+   */
+  @VisibleForTesting
+  void checkAndAddAces(ACE[] securityAces, List<ACE> aceList,
+      List<ACE> aceDenyList, Predicate<ACE> predicate) throws IOException {
+    if (securityAces == null) {
+      LOGGER.warning("Cannot process ACL because of null ACL on " + file);
+      return;
+    }
+    for (ACE ace : securityAces) {
+      if (predicate.apply(ace)) {
+        checkAndAddAce(ace, aceList, aceDenyList);
+      }
+    }
+  }
+}
---
+++ 3.0.x/3.0.6-RC2/projects/file-system-connector/source/javatests/com/google/enterprise/connector/filesystem/MockDirectoryBuilder.java
@@ -0,0 +1,132 @@
+// Copyright 2012 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.enterprise.connector.filesystem;
+
+import com.google.common.annotations.VisibleForTesting;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Test utility that builds a up directory structure of MockReadonlyFiles
+ * for use by other tests.  As the directory structure is being constructed,
+ * a list of expected results is also being constructed.
+ * The ConfigureFile parameter is used to perform two tasks as files and
+ * directories are added to the hierarchy:
+ * <ol>
+ *   <li>The file or directory may be explicity configured using the
+ *       various MockReadonlyFile setters.</li>
+ *   <li>The file or directory may be included or excluded from the list
+ *       of expected results that is being assembled.</li>
+ * </ol>
+ */
+public class MockDirectoryBuilder {
+
+  @VisibleForTesting
+  List<MockReadonlyFile> expected = new ArrayList<MockReadonlyFile>();
+
+  public static final ConfigureFile CONFIGURE_FILE_ALL = new ConfigureFile() {
+      public boolean configure(MockReadonlyFile file) {
+        // Do not alter file configuration, add it to expected results.
+        return true;
+      }
+    };
+
+  public static final ConfigureFile CONFIGURE_FILE_NONE = new ConfigureFile() {
+      public boolean configure(MockReadonlyFile file) {
+        // Do not alter file configuration, do not add it to expected results.
+        return false;
+      }
+    };
+
+
+  /** Used to configure test ReadonlyFiles before they are traversed. */
+  public interface ConfigureFile {
+    /**
+     * Allows the supplied file to be configured as the test data is
+     * being built.
+     *
+     * @param file a MockReadonlyFile that may be configured via its setters.
+     * @return true if the file is expected in the traversal results; false
+     *     if the file is not expected to be included in the traversal results.
+     */
+    public boolean configure(MockReadonlyFile file) throws Exception;
+  }
+
+  /**
+   * Return the list of expected files based upon ConfigureFile rules while
+   * building the directory, sorted in lexigraphical order.
+   */
+  public List<MockReadonlyFile> getExpected() {
+    Collections.sort(expected, new Comparator<MockReadonlyFile>() {
+      /* @Override */
+      public int compare(MockReadonlyFile o1, MockReadonlyFile o2) {
+        return o1.getPath().compareTo(o2.getPath());
+      }
+    });
+
+    return expected;
+  }
+
+  /**
+   * Builds a small directory tree to traverse.
+   *
+   * @param configureFile used to configure dirs and files added to tree
+   * @param expected a List to which expected results are added
+   * @param parent the parent directory under which to add a subdir;
+   *        or null to create a root dir
+   * @param dirName the name of the directory to create
+   * @param fileNames names of regular files to add to the directory
+   * @return the directory
+   */
+  public MockReadonlyFile addDir(MockReadonlyFile parent, String dirName,
+      String... fileNames) throws Exception {
+    return addDir(CONFIGURE_FILE_ALL, parent, dirName, fileNames);
+  }
+
+  /**
+   * Builds a small directory tree to traverse.
+   *
+   * @param configureFile used to configure dirs and files added to tree
+   * @param expected a List to which expected results are added
+   * @param parent the parent directory under which to add a subdir;
+   *        or null to create a root dir
+   * @param dirName the name of the directory to create
+   * @param fileNames names of regular files to add to the directory
+   * @return the directory
+   */
+  public MockReadonlyFile addDir(ConfigureFile configureFile,
+      MockReadonlyFile parent, String dirName, String... fileNames)
+      throws Exception {
+    MockReadonlyFile dir;
+    if (parent == null) {
+      dir = MockReadonlyFile.createRoot(dirName);
+    } else {
+      dir = parent.addSubdir(dirName);
+    }
+    if (configureFile.configure(dir)) {
+      expected.add(dir);
+    }
+    for (String name : fileNames) {
+      MockReadonlyFile f = dir.addFile(name, name + "_data");
+      if (configureFile.configure(f)) {
+        expected.add(f);
+      }
+    }
+    return dir;
+  }
+}
+
