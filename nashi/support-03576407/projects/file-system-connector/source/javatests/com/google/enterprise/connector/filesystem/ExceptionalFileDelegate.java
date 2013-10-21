632cf34fa644661c55df3d83cb054c7096ac1209
Takeshi Yamanashi <nashi@google.com>
2013-10-21 13:26:18 +0000

    Branch to provide X build for support case 03576407

---
+++ nashi/support-03576407/projects/file-system-connector/source/javatests/com/google/enterprise/connector/filesystem/ExceptionalFileDelegate.java
@@ -0,0 +1,137 @@
+// Copyright 2012 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.enterprise.connector.filesystem;
+
+import java.io.InputStream;
+import java.io.IOException;
+
+/**
+ * A {@link FileDelegate} implementation that wraps another FileDelegate,
+ * but throws IOExceptions for testing.
+ */
+public class ExceptionalFileDelegate implements FileDelegate {
+
+  /**
+   * Locations from where ExceptionalFileDelegate may throw its IOExceptions.
+   */
+  public static enum Where { NONE, ALL, EXISTS, IS_DIRECTORY, IS_FILE, LENGTH,
+      LAST_MODIFIED, CAN_READ, IS_HIDDEN, LIST, GET_INPUT_STREAM }
+
+  private final FileDelegate delegate;
+  private Where where;
+
+  public ExceptionalFileDelegate(FileDelegate delegate) {
+    this(delegate, Where.NONE);
+  }
+
+  public ExceptionalFileDelegate(FileDelegate delegate, Where where) {
+    this.delegate = delegate;
+    setWhere(where);
+  }
+
+  public synchronized void setWhere(Where where) {
+    this.where = where;
+  }
+
+  /** If we are supposed to throw an IOException, throw it. */
+  private synchronized void maybeThrowIOException(Where whereNow)
+      throws IOException {
+    if (where == Where.ALL) {
+      // For testing detectServerDown() handling.
+      throw new IOException("Server Down");
+    } else if (where == whereNow) {
+      throw new IOException("Test Exception");
+    }
+  }
+
+  /* @Override */
+  public String getPath() {
+    return delegate.getPath();
+  }
+
+  /* @Override */
+  public String getName() {
+    return delegate.getName();
+  }
+
+  /* @Override */
+  public String getParent() {
+    return delegate.getParent();
+  }
+
+  /* @Override */
+  public boolean exists() throws IOException {
+    maybeThrowIOException(Where.EXISTS);
+    return delegate.exists();
+  }
+
+  /* @Override */
+  public boolean isDirectory() throws IOException {
+    maybeThrowIOException(Where.IS_DIRECTORY);
+    return delegate.isDirectory();
+  }
+
+  /* @Override */
+  public boolean isFile() throws IOException {
+    maybeThrowIOException(Where.IS_FILE);
+    return delegate.isFile();
+  }
+
+  /* @Override */
+  public long length() throws IOException {
+    maybeThrowIOException(Where.LENGTH);
+    return delegate.length();
+  }
+
+  @Override
+  public long lastModified() throws IOException {
+    maybeThrowIOException(Where.LAST_MODIFIED);
+    return delegate.lastModified();
+  }
+
+  /* @Override */
+  public boolean canRead() throws IOException {
+    maybeThrowIOException(Where.CAN_READ);
+    return delegate.canRead();
+  }
+
+  @Override
+  public boolean isHidden() throws IOException {
+    maybeThrowIOException(Where.IS_HIDDEN);
+    return delegate.isHidden();
+  }
+
+  /* @Override */
+  public String[] list() throws IOException {
+    maybeThrowIOException(Where.LIST);
+    return delegate.list();
+  }
+
+  /* @Override */
+  public InputStream getInputStream() throws IOException {
+    maybeThrowIOException(Where.GET_INPUT_STREAM);
+    return delegate.getInputStream();
+  }
+
+  /* @Override */
+  public boolean equals(Object obj) {
+    return delegate.equals(obj);
+  }
+
+  /* @Override */
+  public int hashCode() {
+    return delegate.hashCode();
+  }
+}
---
+++ nashi/support-03576407/projects/jcifs/GoogleReadMe.txt
@@ -0,0 +1,52 @@
+Googlers,
+
+If you modify JCIFS here, please follow these guidelines:
+
+0) Make sure you define a build.bootclasspath property in your
+   ${user.home}/google-enterprise-connector-filesystem.properties
+   file that points at a Java 1.5 boot classpath.  This allows
+   the jcifs.jar file to load correctly in deployments running
+   Java 1.5 (which we currently still support).
+
+1) When building, use the ant targets "clean" and "jar".
+   Avoid the targets "all", "jcifs", and "distrib", as they will
+   rebuild the all the docs which will show up as changed.
+   (If your change would significantly change the javadoc,
+   commit the code changes separately, then build the "javadoc"
+   target and commit the doc separately.)
+
+2) Bump the version at the top of build.xml
+   (for instance 1.3.15.2 -> 1.3.15.3).
+   I've been using the 4th segment of the
+   version string to track Google modifications.
+
+3) Add a couple of words describing your change
+   to the "Google-Modified" manifest entry in
+   the "jar" target of build.xml
+
+4) Commit your changes, and the above, to the
+   subversion repository from the projects/jcifs
+   directory.  This isolates commits to the
+   jcifs project from commits to the connector
+   project.
+
+5) Copy the the new jcifs-{$version}.jar file to
+   projects/file-system-connector/third-party/prod/jcifs.jar
+   and commit that.  (Dropping the version from the
+   jar file name makes it easier to drop-in upgrade
+   the connectors.)
+
+6) Also add the new jcifs.jar file to the Sharepoint
+   projects/sharepoint/lib directory and commit it.
+   Both connectors use jcifs.jar and they need to be
+   kept in sync.
+   
+The version string and manifest entries especially
+make it easier to track Google modifications to 
+third party libraries.
+
+You can easily see all the commits to the jcifs
+project from the codesite:
+
+http://code.google.com/p/google-enterprise-connector-file-system/source/list?path=trunk/projects/jcifs
+
